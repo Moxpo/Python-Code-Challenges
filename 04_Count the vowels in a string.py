d9f5dba1fc7a3aa49568db1c661996766b30dc33
Gregor <105127647+Moxpo@users.noreply.github.com>
2022-11-19 23:30:49 +0000

    Update 04_Count the vowels in a string.py
    pi test

--- 04_Count the vowels in a string.py
+++ 04_Count the vowels in a string.py
@@ -1,2 +1,4 @@
 #   Create a function in Python that accepts a single word and returns the number of vowels in that word. In this
 #   function, only a, e, i, o, and u will be counted as vowels — not y.
+
+#   This edit to test git push origin on pi
